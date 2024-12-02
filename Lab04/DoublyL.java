354921dfa05bd2798a435cb471c5b45d647acc7a
Ghulam Ali <f23ari14@aror.edu.pk>
2024-12-02 17:09:51 +0500

    Lab03

---
+++ Lab04/DoublyL.java
@@ -0,0 +1,207 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ga408
+ */
+public class DoublyL {
+    class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+    Node head;
+    Node tail;
+    
+    void addAtStart(int data){
+        
+        Node newNode = new Node(data) ;
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+    void addAtEnd(int data){
+    
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+    void addAtPos(int data, int position){
+    
+        Node newNode=new Node(data);
+        Node current=head;
+        
+        if(position < 0){
+            System.out.println("invalid");
+        }
+        else if(position == 0){
+            addAtStart(data);
+        }
+        else if (current == tail) {
+            addAtEnd(data);
+        } else {
+            newNode.next = current.next;
+            newNode.prev = current;
+            if (current.next != null) {
+                current.next.prev = newNode;
+            }
+            current.next = newNode;
+        }
+        
+    }
+    void deleteFromStart() {
+        if (head == null) {
+            System.out.println("List is null");
+            return;
+        }
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+    }
+    void deleteFromEnd() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
+    void deleteByValue(int data){
+        
+        Node current = head;
+        
+        while (current != null) {
+            
+            if (current.data == data) {
+                
+                if (current == head) {
+                    deleteFromStart();
+                    
+                } else if (current == tail) {
+                    deleteFromEnd();
+                    
+                } else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                return;
+            }
+            current = current.next;
+        }
+        System.out.println("Value is not available");
+    
+    }
+    void displayFromStart(){
+    
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    void displayFromEnd(){
+    
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.prev;
+        }
+        System.out.println("null");
+        
+    }
+        
+    void search(int value){
+        
+        Node current = head;
+        int position = 0;
+        while (current != null) {
+            if (current.data == value) {
+                System.out.println("Value found at position: " + position);
+                return;
+            }
+            current = current.next;
+            position++;
+        }
+        System.out.println("not found");
+    }
+    
+    void reverse() {
+        Node current = head;
+        Node temp = null;
+        
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        if (temp != null) {
+            head = temp.prev;
+        }
+    }
+    
+    void countNodes(){
+    
+        Node current=head;
+        int count=0;
+    
+        while(current != null){
+        count++;
+        current=current.next;
+        }
+        System.out.println("total values in List :"+ count);
+    }
+    
+    public static void main(String[] args) {
+    
+        DoublyL list=new DoublyL();
+        
+        list.addAtStart(10);
+        list.addAtPos(30, 1);
+        list.addAtPos(35, 2);
+        list.addAtPos(45, 3);
+        list.addAtPos(50, 4);
+        list.addAtEnd(40);
+        
+        list.displayFromStart();
+//        list.displayFromEnd();
+        list.deleteByValue(10);
+//        list.reverse();
+//        list.search(20);
+        list.displayFromStart();
+        list.countNodes();
+        
+    }
+
+    
+}
---
+++ Lab07_08/BinaryT.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ga408
+ */
+import java.util.LinkedList;
+import java.util.Queue;
+
+class BTNode {
+
+    int data;
+    BTNode left;
+    BTNode right;
+
+    public BTNode(int data) {
+        this.data = data;
+        left=right=null;
+    }
+    
+    public void insertLeftChild(BTNode parent , BTNode child){
+    parent.left = child;
+    }
+    
+    public void insertRightChild(BTNode parent , BTNode child){
+    parent.right = child;
+    }
+     
+}
+public class BinaryT {
+      BTNode root;
+
+    public BinaryT(int data) {
+        root= new BTNode(data);
+    }
+    public void inOrder(BTNode root ){ // left - root - right
+        
+        if(root==null){
+        return;
+        }
+        
+        inOrder(root.left);
+        System.out.print(" " + root.data);
+        inOrder(root.right);
+    
+    }
+    public void levelOrder(BTNode root){
+        Queue<BTNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            BTNode cur = queue.remove();
+            System.out.print(" "+ cur.data);
+            
+            if(cur.left!=null){
+                queue.add(cur.left);
+            }
+            if(cur.right!=null){
+                queue.add(cur.right);
+            }
+        }
+    }
+        
+    
+        public void preOrder(BTNode root){ // root-left-right
+        
+        if(root==null){
+        return;
+        }
+        System.out.print(" " + root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+        
+         public void postOrder(BTNode root){ // left-right-root
+        
+        if(root==null){
+        return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(" " + root.data);
+    }
+         
+    
+        
+        
+    public static void main(String[] args) {
+        BinaryT n1 = new BinaryT(1);
+        System.out.println(n1.root.data);
+        BTNode n2 = new BTNode(2);
+        BTNode n3 = new BTNode(3);
+        
+        n1.root.insertLeftChild(n1.root, n2);
+        n1.root.insertRightChild(n1.root, n3);
+
+        BTNode n4 = new BTNode(4);
+        BTNode n5 = new BTNode(5);
+        n2.insertLeftChild(n2, n4);
+        n2.insertRightChild(n2, n5);
+        
+         BTNode n6 = new BTNode(6);
+         n3.insertRightChild(n3, n6);
+         System.out.println("In-Order");
+         n1.inOrder(n1.root);
+         System.out.println("\npre-order");
+         n1.preOrder(n1.root);
+         System.out.println("\npost-order");
+         n1.postOrder(n1.root);
+         System.out.println("\nlevel-order");
+         n1.levelOrder(n1.root);
+            
+    }
+        
+    
+    
+}
